6a8aac2858111076bedd320156a57ba78e14f81c
Madda <nobody.youknow@local>
2019-05-02 20:30:17 +0100

    This is magic and will win everything

--- calculator.py
+++ calculator.py
@@ -7,3 +7,15 @@
 # Quotient = Votes / (number_of_seats_gained_by_this_part + 1)
 # Have we run out of seats? Yes then stop
 #                           No: Goto next round
+
+
+def election(number_of_seats, candidates):
+    """Figure out how to win European elections.
+    This party will tell you what you need to do to win European
+    elections. It'll be fine.
+    :param: number_of_seats - integer number of seats in election.
+    :param: candidates - dict of candidate name: votes won
+
+    :return: dict of candidates: seats won
+    """
+
