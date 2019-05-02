0cc6843aea49aa0a3141f289a27bb5885bdc3956
Madda <nobody.youknow@local>
2019-05-02 20:20:44 +0100

    Put the magic formula in, please sign NDAs

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# First round: Quotient = Votes
+#
+# Each round
+# 
+# Highest quotient gets a seat; recalculate that party's quotient
+# Quotient = Votes / (number_of_seats_gained_by_this_part + 1)
+# Have we run out of seats? Yes then stop
+#                           No: Goto next round
